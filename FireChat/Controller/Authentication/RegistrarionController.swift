322c661158bf6ffc0b4cd5121bff8fe6d7f80450
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2022-03-22 15:19:04 -0700

    Add files via upload

--- FireChat/Controller/Authentication/RegistrarionController.swift
+++ FireChat/Controller/Authentication/RegistrarionController.swift
@@ -11,6 +11,8 @@ class RegistrarionController: UIViewController {
     
     // MARK: - Properties
     
+    private var viewModel = RegistrationViewModel()
+    
     private let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         //#imageLiteral(
@@ -18,6 +20,8 @@ class RegistrarionController: UIViewController {
         button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
         button.tintColor = .white
         button.addTarget(self, action: #selector(handleSelectPhoto), for: .touchUpInside)
+        button.imageView?.clipsToBounds = true
+        button.clipsToBounds = true
         return button
     }()
 
@@ -38,7 +42,7 @@ class RegistrarionController: UIViewController {
     private lazy var userNameConteinerView: InputContainerView = {
         
         return InputContainerView(image: #imageLiteral(resourceName: "ic_person_outline_white_2x"),
-                                  textField: usernamelTextField)//#imageLiteral(
+                                  textField: usernameTextField)//#imageLiteral(
         
     }()
     
@@ -49,7 +53,7 @@ class RegistrarionController: UIViewController {
     
     private let emailTextField = CustomTextField(placeholder: "Email")
     private let fullnameTextField = CustomTextField(placeholder: "Full Name")
-    private let usernamelTextField = CustomTextField(placeholder: "Username")
+    private let usernameTextField = CustomTextField(placeholder: "Username")
 
     
     private let passwordTextField: CustomTextField = {
@@ -67,6 +71,7 @@ class RegistrarionController: UIViewController {
         button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
         button.setTitleColor(.white, for: .normal)
         button.setHeight(height: 50)
+        button.isEnabled = false
         return button
     }()
     
@@ -84,6 +89,26 @@ class RegistrarionController: UIViewController {
     
     // MARK: - Selectors
     
+    @objc func textDidChange(sender: UITextField) {
+        if sender == emailTextField {
+            viewModel.email = sender.text
+        } else if sender == passwordTextField {
+            viewModel.password = sender.text
+        } else if sender == fullnameTextField {
+            viewModel.fullname = sender.text
+        } else if sender == usernameTextField {
+            viewModel.username = sender.text
+        }
+        
+        checkFormStatus()
+    }
+    
+    @objc func handleSelectPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
     @objc func handleShowLogin() {
         navigationController?.popViewController(animated: true)
     }
@@ -93,13 +118,10 @@ class RegistrarionController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureNotificationObservers()
     }
     
     // MARK: - Helpers
-    
-    @objc func handleSelectPhoto() {
-        print("Select Photo here...")
-    }
     
     func configureUI() {
         configureGradientLayer()
@@ -126,5 +148,38 @@ class RegistrarionController: UIViewController {
         alreadyHaveAcountButton.anchor(left: view.leftAnchor,
                                     bottom: view.safeAreaLayoutGuide.bottomAnchor,
                                     right: view.rightAnchor, paddingLeft: 32, paddingRight: 32)
+    }
+    
+    func configureNotificationObservers() {
+        emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        fullnameTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        usernameTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension RegistrarionController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as? UIImage
+        plusPhotoButton.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        plusPhotoButton.layer.borderColor = UIColor.white.cgColor
+        plusPhotoButton.layer.borderWidth = 3.0
+        plusPhotoButton.layer.cornerRadius = 200 / 2
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension RegistrarionController: AuthenticationControllerProtocol {
+    func checkFormStatus() {
+        if viewModel.formIsValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        } else {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        }
     }
 }
