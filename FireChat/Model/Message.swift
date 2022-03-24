7a0d77f7615262df58a355cb1b6dc9c9b7f544ec
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2022-03-23 22:26:34 -0700

    Update chat function
    [U] MessageCell.swift 
    [U] ChatController.swift
    
    [A] Message.swift
    ---
    Added the ability to send a Message.

---
+++ FireChat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FireChat
+//
+//  Created by Takafumi Watanabe on 2022-03-23.
+//
+
+import Foundation
+
+struct Message {
+    let text: String
+    let isFromCurrentUser: Bool
+}
