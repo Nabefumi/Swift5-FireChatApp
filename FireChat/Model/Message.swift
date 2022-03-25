4f00d0bc6a0f24e1e0b404330f40693c1a93a9c3
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2022-03-24 21:52:24 -0700

    Update chat message function
    [U] Service.swift
    [U] ChatController.swift
    [A] Constants.swift
    ---
    Firebase Integration

--- FireChat/Model/Message.swift
+++ FireChat/Model/Message.swift
@@ -5,9 +5,23 @@
 //  Created by Takafumi Watanabe on 2022-03-23.
 //
 
-import Foundation
+import Firebase
 
 struct Message {
     let text: String
+    let toId: String
+    let fromId: String
+    var timestamp: Timestamp!
+    var user: User?
+    
     let isFromCurrentUser: Bool
+    
+    init(dictionary: [String: AnyObject]) {
+        self.text = dictionary["text"] as? String ?? ""
+        self.toId = dictionary["toId"] as? String ?? ""
+        self.fromId = dictionary["fromId"] as? String ?? ""
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        
+        self.isFromCurrentUser = fromId == Auth.auth().currentUser?.uid
+    }
 }
