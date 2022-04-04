27a7c76266fbda4f3e94324f2c1844514f4ae20f
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2022-04-04 15:07:11 -0700

    Update chat functions
    [U] Service.swift
    [U] MessageCell.swift
    [U] ConversationsController.swift
    [U] Service.swift
    
    [A] ConversationCell.swift

---
+++ FireChat/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  FireChat
+//
+//  Created by Takafumi Watanabe on 2022-03-24.
+//
+
+import Firebase
+
+let COLLECTION_MESSAGES = Firestore.firestore().collection("messages")
+let COLLECTION_USERS = Firestore.firestore().collection("users")
+
